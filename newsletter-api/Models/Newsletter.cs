24793f6bcac0616be89641105fe09494ef0e11cd
gooddogdevelopment-sys <gooddogdevelopment@gmail.com>
2026-03-30 11:44:03 -0400

    Added max character length annotations to the Newsletter.cs object

--- newsletter-api/Models/Newsletter.cs
+++ newsletter-api/Models/Newsletter.cs
@@ -5,11 +5,21 @@ namespace dotnet_core_api_w_postgres.Models;
 public class Newsletter : BaseEntity
 {
     [MaxLength(60)]
-    public required string Title { get; set; }
+    public required string Title { get; init; }
+
+    [MaxLength(3000)]
     public string? CodeSnippet { get; set; }
+
+    [MaxLength(1000)]
     public required string Overview { get; set; }
+
+    [MaxLength(100)]
     public required string Subject { get; set; }
     public required DateTime SendDate { get; set; }
+
+    [MaxLength(50)]
     public required string AiProvider { get; set; }
+
+    [MaxLength(50)]
     public required string AiModel { get; set; }
 }
